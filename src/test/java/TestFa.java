78a83eb784faa5e094ae5fbe5418114cc0aaed57
Dova81 <33400774+Dova81@users.noreply.github.com>
2019-08-31 15:34:17 -0300

    fafa

---
+++ src/test/java/TestFa.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class TestFa {
+
+    @Before
+    public void setUp(){
+
+    }
+
+
+    @After
+    public void tearDown(){
+
+    }
+
+    @Test
+    public void testIntegro() {
+        assertTrue(true);
+    }
+}
