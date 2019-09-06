9fdea96bcb2a41774b95ba865cc6a3716a40e2ca
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-09-06 19:17:55 -0300

    Tried to run application test

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,14 +1,26 @@
 /*
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class ApplicationTest {
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void contextLoads() {
+    }
+
+    @TestConfiguration
+    static class TestContextConfiguration {
+
     }
 }
 */
