4717a6e489f5bfc016b3930c138b2bd45744e741
Dova81 <33400774+Dova81@users.noreply.github.com>
2019-09-21 15:43:56 -0300

    Este codigo habria que prenderlo fuego
    Hay que llenar el coverage testeando boludeces en account y business

--- src/test/java/ar/edu/unq/dapp/c2a/model/account/AccountTest.java
+++ src/test/java/ar/edu/unq/dapp/c2a/model/account/AccountTest.java
@@ -1,11 +1,17 @@
 package ar.edu.unq.dapp.c2a.model.account;
 
 import ar.edu.unq.dapp.c2a.model.EntityTest;
+import ar.edu.unq.dapp.c2a.model.account.statement.AcreditationStatement;
+import ar.edu.unq.dapp.c2a.model.account.statement.Statement;
 import ar.edu.unq.dapp.c2a.model.order.invoice.Invoice;
 import org.junit.Test;
 
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -62,5 +68,35 @@ public class AccountTest extends EntityTest {
         );
     }
 
+    @Test
+    public void anAccountShouldSetAndGetAndId(){
+        List<Statement> movDeCuenta = new ArrayList<Statement>();
+        Account anAcount = new BasicAccount(aMonetaryAmount(),movDeCuenta );
+        anAcount.setStatements(movDeCuenta);
+        anAcount.setId(100L);
 
+
+        Long IdTest = 100L;
+        Long IdCuenta = anAcount.getId();
+
+        assertEquals(IdTest,IdCuenta);
+
+    }
+
+    @Test
+    public void anStatementShouldSetAndGetId(){
+        List<Statement> movDeCuenta = new ArrayList<Statement>();
+        Statement mov = new AcreditationStatement(aMonetaryAmount());
+        mov.setId(50L);
+        movDeCuenta.add(mov);
+        Account anAcount = new BasicAccount(aMonetaryAmount(),movDeCuenta );
+        anAcount.setStatements(movDeCuenta);
+
+
+        Long IdTest = 50L;
+        Long IdMov = mov.getId();
+
+        assertEquals(IdTest,IdMov);
+
+    }
 }
--- src/test/java/ar/edu/unq/dapp/c2a/model/business/BusinessTest.java
+++ src/test/java/ar/edu/unq/dapp/c2a/model/business/BusinessTest.java
@@ -3,7 +3,12 @@ package ar.edu.unq.dapp.c2a.model.business;
 import ar.edu.unq.dapp.c2a.model.EntityTest;
 import ar.edu.unq.dapp.c2a.model.client.Client;
 import ar.edu.unq.dapp.c2a.model.menu.Menu;
+import ar.edu.unq.dapp.c2a.model.order.Order;
+import ar.edu.unq.dapp.c2a.model.order.invoice.Invoice;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
@@ -52,4 +57,27 @@ public class BusinessTest extends EntityTest {
                 aBusiness.getInvoices().size()
         );
     }
+
+    @Test
+    public void aBusinessShouldHaveAnID() {
+        Collection<Invoice> pagos = new ArrayList<Invoice>();
+        Collection<Order> ordenes = new ArrayList<Order>();
+        Menu aMenu = aMenu();
+        Business aBusiness = aMenu.getBusiness();
+        aBusiness.setInvoices(pagos);
+        aBusiness.setPendingOrders(ordenes);
+
+        aBusiness.setId(50L);
+
+        Long IdTest = 50L;
+        Long IdMov = aBusiness.getId();
+
+
+        assertEquals(
+                IdTest,
+                IdMov
+        );
+    }
+
+
 }
