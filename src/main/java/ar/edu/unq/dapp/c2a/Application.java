bcd4c50358741f92f1d38f58be553e6903d963eb
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-09-26 11:22:43 -0300

    Update Application.java

--- src/main/java/ar/edu/unq/dapp/c2a/Application.java
+++ src/main/java/ar/edu/unq/dapp/c2a/Application.java
@@ -16,16 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 import javax.money.Monetary;
 import java.util.Calendar;
 
 @SpringBootApplication
-@PropertySources({
-        @PropertySource("classpath:application.properties")
-})
 public class Application {
 
     public static void main(String[] args) {
