5536c2a2574970395b520879d43c56f8089ec948
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-10-11 11:36:56 -0300

    Added monetary amount object mapper

--- src/main/java/ar/edu/unq/dapp/c2a/Application.java
+++ src/main/java/ar/edu/unq/dapp/c2a/Application.java
@@ -8,14 +8,13 @@ import ar.edu.unq.dapp.c2a.model.menu.Menu;
 import ar.edu.unq.dapp.c2a.model.menu.MenuBuilder;
 import ar.edu.unq.dapp.c2a.model.order.Order;
 import ar.edu.unq.dapp.c2a.model.order.OrderBuilder;
-import ar.edu.unq.dapp.c2a.persistence.business.BusinessDAO;
-import ar.edu.unq.dapp.c2a.persistence.client.ClientDAO;
-import ar.edu.unq.dapp.c2a.persistence.menu.MenuDAO;
 import ar.edu.unq.dapp.c2a.persistence.order.OrderDAO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.zalando.jackson.datatype.money.MoneyModule;
 
 import javax.money.Monetary;
 import java.util.Calendar;
@@ -23,12 +22,15 @@ import java.util.Calendar;
 @SpringBootApplication
 public class Application {
 
+    @Autowired
+    private OrderDAO orderdao;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Autowired
-    private OrderDAO orderdao;
 
     @Bean
     public void load() {
@@ -53,4 +55,8 @@ public class Application {
 
     }
 
+    @Bean
+    public void configureMoneyMapper() {
+        objectMapper.registerModule(new MoneyModule());
+    }
 }
